7f190e81e5990bbc48787b8e4a85fd2299f3c094
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-08-17 16:33:06 +0100

    Adding Firebase User Signup

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,39 +1,83 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react'
 
-import classes from './AuthForm.module.css';
+import classes from './AuthForm.module.css'
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
+  const emailInputRef = useRef()
+  const passwordInputRef = useRef()
+
+  const [isLogin, setIsLogin] = useState(true)
 
   const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
+    setIsLogin((prevState) => !prevState)
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+
+    const enteredEmail = emailInputRef.current.value
+    const enteredPassword = passwordInputRef.current.value
+
+    // optional -> add validation
+
+    if (isLogin) {
+    } else {
+      fetch(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA6v2BMfmvZsV2w8VgxR3TG2PqfIZWluVA ',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+        }
+      ).then((res) => {
+        if (res.ok) {
+          // ...
+        } else {
+          //throw some error
+          res.json().then((data) => {
+            // show an error modal
+            console.log(data)
+          })
+        }
+      })
+    }
+  }
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
+            onClick={switchAuthModeHandler}>
             {isLogin ? 'Create new account' : 'Login with existing account'}
           </button>
         </div>
       </form>
     </section>
-  );
-};
+  )
+}
 
-export default AuthForm;
+export default AuthForm
