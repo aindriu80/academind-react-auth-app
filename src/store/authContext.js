b7b13c45b37544a1e3cc2caf802f2554505d8c39
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-08-19 21:35:38 +0100

    Managing the Auth State with Context - bug with AuthCoontextProvider

---
+++ src/store/authContext.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+const AuthContext = React.createContext({
+  toke: '',
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+})
+
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null)
+
+  const userIsLoggedIn = !!token
+
+  const loginHandler = (token) => {
+    setToken(token)
+  }
+
+  const logoutHandler = () => {
+    setToken(null)
+  }
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logoutout: logoutHandler,
+  }
+
+  return (
+    <AuthContextProvider value={contextValue}>
+      {props.children}
+    </AuthContextProvider>
+  )
+}
+
+export default AuthContext
